9b289e89e9f2c02b6ae181ac4450ae9413772fbb
Marshall Lorhan <mlorhan@gmail.com>
2024-03-02 12:07:42 -0800

    adding places controller

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+
+const express = require('express')
+const app = express()
+
+app.use('/places', require('.controller/places'))
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 page</h1>')
+})
+
+app.listen(process.env.PORT)
+
