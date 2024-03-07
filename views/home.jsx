4bd412aca4584ea79e59cfff46f236d540f91a06
Marshall Lorhan <mlorhan@gmail.com>
2024-03-06 20:58:23 -0800

    updated styling on home page

--- views/home.jsx
+++ views/home.jsx
@@ -5,10 +5,10 @@ function home () {
     return (
         <Def>
             <main>
-                <h1>HOME</h1>
+                <h1>REST-Rant</h1>
                 <div>
                     <img src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
-                    <div>
+                    <div className="text-center">
                         Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
                     </div>
                 </div>
