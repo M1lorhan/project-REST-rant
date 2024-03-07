2a6507ec2d4097a214a5b9d81b7b4642dc8a18e6
Marshall Lorhan <mlorhan@gmail.com>
2024-03-06 20:54:27 -0800

    adding bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ function error404 () {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+                <div>
+                    <img src="/images/the-lucky-neko-rplhB9mYF48-unsplash.jpg" alt="Three Cute Kittens"/>
+                    <div>
+                    Photo by <a href="AUTHOR_LINK">Tuqa Nabi</a> <a href="UNSPLASH_LINK"></a>on Unsplash 
+                    </div>
+                </div>
             </main>
         </Def>
     )
