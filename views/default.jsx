4df3ae8284dccd71f79e458a1417bd5e5f6b36d0
Marshall Lorhan <mlorhan@gmail.com>
2024-03-07 20:23:19 -0800

    added sticky footer

--- views/default.jsx
+++ views/default.jsx
@@ -8,12 +8,27 @@ function Def (html) {
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"/>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous"></link>
                 <link rel="stylesheet" href="/style.css"/>
+                <script src="https://kit.fontawesome.com/f5d4d7cd3c.js" crossorigin="anonymous"></script>
             </head>
             <body>
+                <div className="content">
+                <div className="content-inside">
                 {html.children}
+                </div>
+                </div>
             </body>
+            <footer className="footer">
+                <div>REST-Rant</div>
+            <i class="fa-brands fa-x-twitter"></i>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-facebook"></i>
+            <i class="fa-brands fa-linkedin"></i>
+            <i class="fa-brands fa-github"></i>
+            </footer>
         </html>
     )
 }
+
+
 
 module.exports = Def
