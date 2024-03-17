0c9927f2390c7dff8171657025777c8e841ecaa5
Marshall Lorhan <mlorhan@gmail.com>
2024-03-16 19:13:42 -0700

    added helper method

--- models/places.js
+++ models/places.js
@@ -1,25 +1,28 @@
-/*module.exports = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/emy-XoByiBymX20-unsplash.jpg' 
-}, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/heather-m-edwards-TsSvA6l6fTc-unsplash.jpg'
-}]*/
-
 const mongoose = require('mongoose')
-const placeScehma = new mongoose.Schema({
-    name: {type: String, required: true},
+
+const placeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
     pic: String,
-    cuisines: {type: String, required: true},
-    city: {type: String, default: 'Anytown'},
-    state: {type: String, default: 'USA'},
+    cuisines: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        default: 'Anytown'
+    },
+    state: {
+        type: String,
+        default: 'USA'
+    },
     founded: Number
 })
 
-module.exports = mongoose.model('Place', placeScehma)
+placeSchema.methods.showEstablished = function() {
+    return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}.`
+}
+
+module.exports = mongoose.model('Place', placeSchema)
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,15 +5,22 @@ function show (data) {
     return (
         <Def>
             <main>
-                <h1>{ data.place.name }</h1>
+                <div className="row">
+                <div className="col-sm-6">
                 <img src={ data.place.pic } alt={ data.place.name }></img>
-                <h2>Rating</h2>
-                <p>Not Rated</p>
-                <h2>Comments</h2>
-                <p>No Comments yet!</p>
-                <h2>Description</h2>
-                <p>Located in { data.place.city }, { data.place.state }</p>
-                <p>Serving { data.place.cuisines }</p>
+                <h3>Located in { data.place.city }, { data.place.state }</h3>
+                </div>
+                <div className="col-sm-6">
+                    <h1>{ data.place.name }</h1>
+                    <h2>Rating</h2>
+                    <p>Not Rated</p>
+                    <h2>Description</h2>
+                <h3>
+                    { data.place.showEstablished() }
+                </h3>
+                <h4>
+                    Serving { data.place.cuisines }
+                </h4>
                 <a href={`/places/${data.id}/edit`} className="btn btn-warning">
                     Edit <i class="fa-solid fa-pencil"></i>
                     </a>
@@ -22,6 +29,10 @@ function show (data) {
                     Delete <i class="fa-solid fa-trash"></i>
                     </button>
                 </form>
+                </div>
+                </div>
+                <h2>Comments</h2>
+                    <p>No Comments yet!</p>
             </main>
         </Def>
     )
